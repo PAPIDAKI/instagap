73479f7b6d2a6c88585a2837eca891974a3f9e1b
Manolis Papidakis <papidakis@gmail.com>
2015-03-09 10:43:29 +0200

    sti activity working unested

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,11 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
-
   respond_to :html
 
-  def index
-		@activities = Activity.all
+  before_action :set_type
 
+  def index
+		@activities=type_class.all
   end
 
   def show
@@ -13,23 +13,16 @@ class ActivitiesController < ApplicationController
   end
 
   def new
-		#@registration=Registration.find(params[:registration_id])
-    #@activity = @registration.activities.new
-		@activity=Activity.new
-		@activity.type=params[:type]
-
-
+		@activity=type_class.new
   end
 
   def create
-	  #@registration=Registration.find(params[:registration_id])
-	  #@activity = @registration.activities.new(activity_params)
-	  #@group=@registration.group
-	  @activity=Activity.create(activity_params)
 
-	  if @activity.save
-		  redirect_to
-		  @activities
+	  @activity=type_class.new(activity_params)
+
+
+		if @activity.save
+		  redirect_to activities_path,notice:"#{type} was succesfully created."
 	  else
 		  render :new
 	  end
@@ -55,11 +48,6 @@ class ActivitiesController < ApplicationController
 
 
   private
-    def edit_activity_params
-			type=@activity.type
-			params.require(type.underscore.to_sym).permit(:date,:type,:note,:operator,:persons,:approved_by)
-
-    end
 
 
     def set_type
@@ -67,21 +55,28 @@ class ActivitiesController < ApplicationController
     end
 
     def type
-			Activity.types.include?(params[:type])? params[:type] :"Activity"
-
+			Activity.types.include?(params[:type])? params[:type] : "Activity"
     end
     def type_class
 			type.constantize
     end
+
+
     def set_registration
 			@registration=Registration.find(params[:registration_id])
     end
-    def set_activity
 
-      @activity = Activity.find(params[:id])
+    def set_activity
+			@activity=Activity.find(params[:id])
     end
 
     def activity_params
-	    params.require(:activity).permit(:date, :type, :note, :operator, :persons, :approved_by, :production_id)
+			params.require(type.underscore.to_sym).permit(:date, :type, :note, :operator, :persons, :approved_by,:production_id)
+    end
+
+    def edit_activity_params
+	  type=@activity.type
+	  params.require(type.underscore.to_sym).permit(:date,:type,:note,:operator,:persons,:approved_by,:production_id)
+
     end
 end
