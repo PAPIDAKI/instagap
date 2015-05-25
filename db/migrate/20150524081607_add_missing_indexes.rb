549f605b44614902a69e7eeb88af6fc5688eab12
Manolis Papidakis <papidakis@gmail.com>
2015-05-25 14:42:38 +0300

    fu

--- db/migrate/20150524081607_add_missing_indexes.rb
+++ db/migrate/20150524081607_add_missing_indexes.rb
@@ -24,7 +24,7 @@ class AddMissingIndexes < ActiveRecord::Migration
         add_index :mfarmkasts, :mfarmako_id
     # add_index :mepembashes, :mcrop_id
     # add_index :mepembashes, :mfarmako_id
-        add_index :mepembashes, [:mfarmako_id, :mcrop_id]
+    # add_index :mepembashes, [:mfarmako_id, :mcrop_id]
         add_index :mastfarms, :mdosh_id
         add_index :mastfarms, :masthenium_id
         # add_index :mastfarms, :mfarmkast_id
