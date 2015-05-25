faac4b775a99039f2ec4184e6e37f10b3411e8af
Manolis Papidakis <papidakis@gmail.com>
2015-05-25 14:13:18 +0300

    stubout counter to counterable_id migration for heroku

--- db/migrate/20150505163408_turn_mastfarm_to_polymorphic.rb
+++ db/migrate/20150505163408_turn_mastfarm_to_polymorphic.rb
@@ -1,7 +1,7 @@
 class TurnMastfarmToPolymorphic < ActiveRecord::Migration
   def up
   	add_column :mastfarms,:counterable_type,:string
-  	rename_column :mastfarms,:counter,:counterable_id
+  	# rename_column :mastfarms,:counter,:counterable_id
   end
    def down
    	rename_column :mastfarms,:counterable_id,:counter
